5796d0fb69cbc3ed8578fbbe0a3402630d048a00
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-06-19 00:44:57 +0530

    redesigned nav bar and toggle button

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const menuBar = document.querySelector(".menu-bar");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  menuBar.classList.toggle(".active-bar");
-});
 
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}));
